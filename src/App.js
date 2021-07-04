0cf8fe1ec2a666c989a0746ebae9efc27f579e71
funduleadiana <diana.fundulea@yahoo.com>
2021-07-04 19:12:06 +0100

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {useForm}  from './useForm';
-import { UseFetch } from './UseFetch';
+import { useFetch } from './UseFetch';
 import './App.css';
 
 function App() {
@@ -9,13 +9,13 @@ function App() {
   const [count, setCount] = useState(()=> 
   JSON.parse(localStorage.getItem('count'))
   );
-  const {data, loading} = UseFetch(`http://numbersapi.com/42`);
+  const {data, loading} = useFetch(`http://numbersapi.com/${count}/trivia`);
 
   useEffect(()=> localStorage.setItem('count', JSON.stringify(count)), [count]);
   
   return (
     <div className="App">
-      <div></div>
+      <div>{!data ? "loading.." : data}</div>
       <div>count: {count}</div>
       <button onClick={()=> setCount(c=> c+1)}>Increment count</button>
       <input name="email" value={values.email} onChange={handleChange}/>
