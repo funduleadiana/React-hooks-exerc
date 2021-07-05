4895958c688c096f8ce2eb8de6f5c84647c587e3
funduleadiana <diana.fundulea@yahoo.com>
2021-07-05 10:00:04 +0100

    tracks the number of renders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import {useForm}  from './useForm';
 import { useFetch } from './UseFetch';
+import {Hello} from './Hello'
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
   );
   const {data, loading} = useFetch(`http://numbersapi.com/${count}/trivia`);
   const inputRef = useRef();
+  const [showHello, setShowHello] = useState(true)
   useEffect(()=> localStorage.setItem('count', JSON.stringify(count)), [count]);
   
   return (
@@ -18,6 +20,8 @@ function App() {
       <div>{!data ? "loading.." : data}</div>
       <div>count: {count}</div>
       <button onClick={()=> setCount(c=> c+1)}>Increment count</button>
+      <button onClick={()=> setShowHello(!showHello)}>toggle</button>
+      {showHello && <Hello/>}
       <input ref={inputRef} name="email" value={values.email} onChange={handleChange}/>
       
       <input type="password" name="password" value={values.password} onChange={handleChange}/>
